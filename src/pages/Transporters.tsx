ac7646fe9cf6d80743e404bef5b4cc2397fc00b1
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2025-04-22 10:51:59 +0530

    fix mobile2

--- src/pages/Transporters.tsx
+++ src/pages/Transporters.tsx
@@ -112,7 +112,7 @@ const Transporters = () => {
   const mobileColumns = isMobile
     ? columns.filter((col) => {
         if (typeof col.header === 'string') {
-          return ["Name", "Status", "Actions"].includes(col.header);
+          return ["Name","GST Number","Contact Person","Phone", "Status", "Actions"].includes(col.header);
         }
         return false;
       })
